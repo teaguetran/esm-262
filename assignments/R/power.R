bf01a3506b0ca85095edbcca5f3eb9eae078345a
camerynbrock <cbrock@bren.ucsb.edu>
2021-02-17 10:34:17 -0800

    original commit after class - created function and calculated power for four speeds

---
+++ assignments/R/power.R
@@ -0,0 +1,15 @@
+# compute power required to keep a car moving at a given speed
+## c_r is rolling resistance coefficient (typically 0.015)
+## c_d is aerodynamic resistance coefficient (typically 0.3)
+## v is vehicle speed (m/s) 
+## m is vehicle mass (kg)
+## a is surface area (m^2)
+## g is acceleration due to gravity (9.8 m/s^2)
+## p_a is air density (1.2 kg/m^3)
+## power is power (watts)
+
+power <- function(m, v, a, 
+                  g = 9.8, p_a = 1.2, 
+                  c_d = 0.3, c_r = 0.015) {
+  power = c_r * m * g * v * 0.5 * a * p_a * v^3 
+}
---
+++ working/R/power_gen_orig.R
@@ -0,0 +1,4 @@
+power_gen_orig = function(a,b) {
+  add = a+b
+  return(add)
+}
